64904c3549fc8de76a93eae1317134997688669c
Trevor Spreadbury <trevorspreadbury@gmail.com>
2024-09-24 10:04:55 -0500

    fix paths of post_gen_project hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,9 +14,9 @@ if not use_docker:
     os.remove("Dockerfile")
 
 if not create_example_files:
-    os.remove("{{ cookiecutter.code_directory }}/pipeline_example.py")
+    os.remove("src/{{ cookiecutter.code_directory }}/pipeline_example.py")
     os.remove(
-        "{{ cookiecutter.code_directory }}/preprocess_util_lib_example.py"
+        "src/{{ cookiecutter.code_directory }}/preprocess_util_lib_example.py"
     )
     os.remove("notebooks/Test.ipynb")
 
